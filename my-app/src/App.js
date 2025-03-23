cbd76c1a35aa1f90ebbd62059b8cad8febe319a2
isluel <isluel@nate.com>
2025-03-23 18:51:14 +0900

    add timeout.yaml

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction11
+          Learn GithubAction1
         </a>
       </header>
     </div>
